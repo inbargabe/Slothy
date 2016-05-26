23a6c2a29188b20bced14a78a39368303f3202fb
Inbar Gabe <amber.gabe@gmail.com>
2016-05-26 12:25:13 +0300

    Added 3 more stages

---
+++ Assets/Scripts/ButtonsControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonsControl : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	public void Back(){
+		SceneManager.LoadScene ("Levels");
+
+	}
+	public void Level1(){
+		SceneManager.LoadScene ("Stage1");
+
+	}
+	public void Level2(){
+		SceneManager.LoadScene ("Stage2");
+
+	}
+	public void Level3(){
+		SceneManager.LoadScene ("Stage3");
+
+	}
+	public void Level4(){
+		SceneManager.LoadScene ("Stage4");
+
+	}
+	public void Level5(){
+		SceneManager.LoadScene ("Stage5");
+	}
+	public void Level6(){
+		SceneManager.LoadScene ("Stage6");
+	}
+	public void Level7(){
+		SceneManager.LoadScene ("Stage7");
+	}
+	public void Level8(){
+		SceneManager.LoadScene ("Stage8");
+	}
+}
---
+++ Assets/Scripts/fireball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireball : MonoBehaviour {
+
+	public float leftRightSpeed;
+	public GameObject ball;
+	public float spawnTime = 5f;
+	public bool isGoingRight;
+
+
+	// Use this for initialization
+	void Start () {
+		//Move (isGoingRight);
+		InvokeRepeating ("SpawnBall", spawnTime, spawnTime);
+
+	}
+		    
+	void SpawnBall()
+	{
+		var newBall = GameObject.Instantiate(ball);
+	}
+
+	/**void Move(bool isGoingRight)
+	{
+		//float movementX = isGoingRight ? leftRightSpeed : -leftRightSpeed;
+		float movementX = -leftRightSpeed;
+		transform.Translate (new Vector3 (movementX * Time.deltaTime, 0, 0));
+	}**/
+
+	/*
+	void OnCollisionEnter2D(Collision2D objectCollision)
+	{
+		if (objectCollision.gameObject.tag == "Player") {
+			//	Destroy (gameObject);
+			Application.LoadLevel("LoseLevel");
+
+		}
+	}
+	*/
+}
