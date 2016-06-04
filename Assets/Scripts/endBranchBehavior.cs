cbf16b305c4111927a1f15bc2202bf5d5b9313a9
Inbar Gabe <amber.gabe@gmail.com>
2016-06-04 22:47:04 +0300

    finished 5 stages to be perfect

---
+++ Assets/Scripts/endBranchBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endBranchBehavior : MonoBehaviour {
+
+	public GameObject sloth;
+	public GameObject branch1;
+
+	BoxCollider2D branchCollider;
+
+	// Use this for initialization
+	void Start () {
+		branchCollider = branch1.GetComponent<BoxCollider2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sloth.transform.position.y - transform.position.y > 0.6) {
+			branchCollider.isTrigger = false;
+		}
+	}
+}
