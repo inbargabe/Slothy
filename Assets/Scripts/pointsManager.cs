55b9e91267b558a60c636aa713d9008d68a18516
Inbar Gabe <amber.gabe@gmail.com>
2016-06-12 15:40:25 +0300

    added canves

--- Assets/Scripts/pointsManager.cs
+++ Assets/Scripts/pointsManager.cs
@@ -8,6 +8,7 @@ public class pointsManager : MonoBehaviour {
 	public static int flies;
 
 	public Image flyBar;
+	public Image flyBarEndOfscene;
 
 	// Use this for initialization
 	void Start () {
@@ -20,22 +21,27 @@ public class pointsManager : MonoBehaviour {
 
 		case 0:
 			flyBar.fillAmount = 0f;
+			flyBarEndOfscene.fillAmount = 0f;
 			break;
 		
 		case 1:
 			flyBar.fillAmount = 0.33f;
+			flyBarEndOfscene.fillAmount = 0.33f;
 			break;
 
 		case 2:
 			flyBar.fillAmount = 0.67f;
+			flyBarEndOfscene.fillAmount = 0.67f;
 			break;
 
 		case 3:
 			flyBar.fillAmount = 1f;
+			flyBarEndOfscene.fillAmount = 1f;
 			break;
 
 		default:
 			flyBar.fillAmount = 0f;
+			flyBarEndOfscene.fillAmount = 0f;
 			break;
 		}
 	}
