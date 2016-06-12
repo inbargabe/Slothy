eb969e3496e51a439d11736e268141a2394464fc
Inbar Gabe <amber.gabe@gmail.com>
2016-06-12 13:22:34 +0300

    Added sounds

--- Assets/Scripts/FlyBehavior.cs
+++ Assets/Scripts/FlyBehavior.cs
@@ -4,30 +4,19 @@ using System.Collections;
 public class FlyBehavior : MonoBehaviour {
 
 	public SpriteRenderer flySpriteRenderer;
-
-
-
-
-
+	public bool playerTouchedFly;
 
 	// Use this for initialization
 	void Start () {
 		flySpriteRenderer = GetComponent<SpriteRenderer> ();
-
-
+		playerTouchedFly = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D objectCollision) {
-
 		if (objectCollision.gameObject.tag == "Player") {
 			pointsManager.flies++;
-
 			Destroy (gameObject);
-				
-				
-			
-			}
-
+			playerTouchedFly = true;
 		}
 	}
-
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 	public AudioClip flySound1;
-
 	public AudioSource audio;
+
+	FlyBehavior m_flyBehavior;
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
@@ -12,6 +13,11 @@ public class SoundManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (m_flyBehavior.playerTouchedFly) {
+			audio.PlayOneShot (flySound1, 0.7f);
+			m_flyBehavior.playerTouchedFly = false;
+		}
 		
 	}
 	void OnTriggerEnter2D(Collider2D objectCollision) {
